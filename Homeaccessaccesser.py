b880acdb5b2b11482448e4ed0ccc52f7dd5238df
SparkleFox715 <57511754+SparkleFox715@users.noreply.github.com>
2021-10-18 08:00:24 -0500

    Update Homeaccessaccesser.py

--- Homeaccessaccesser.py
+++ Homeaccessaccesser.py
@@ -1,1 +1,10 @@
-print(hello)
+import requests
+page = requests.get("https://homeaccess.katyisd.org/HomeAccess/Account/LogOn?ReturnUrl=%2fHomeAccess%2f")
+url = 'https://homeaccess.katyisd.org/HomeAccess/Account/LogOn?ReturnUrl=%2fHomeAccess%2f'
+print(page.content)
+values = {
+    'username':'m0904314',
+    'password':'pasew'
+}
+r=requests.post(url, data=values)
+print(r.content)
